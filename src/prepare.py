4ba426177f83f69fb3fd7f1ab31b0a21f277069a
arielherself <ariel@arielherself.xyz>
2023-06-05 13:14:48 +0800

    optimize 'q' for quit change speed based on fixed ratio

--- src/prepare.py
+++ src/prepare.py
@@ -5,23 +5,20 @@ from misc import getch
 V_BASE = 30
 
 async def main():
-    try:
-        recorder = GPIO.Recorder('actions.pkl', verbosed=True)
-        while 1:
-            p = getch()
-            if p == 'w':
-                await recorder.set_speed_and_dump(V_BASE, V_BASE)
-            elif p == 's':
-                await recorder.set_speed_and_dump(0, 0)
-                await recorder.finish()
-                break
-            elif p == 'a':
-                await recorder.set_speed_and_dump(recorder.lspeed-10, recorder.rspeed)
-            elif p == 'd':
-                await recorder.set_speed_and_dump(recorder.lspeed, recorder.rspeed-10)
-    except KeyboardInterrupt:
-        print("Unexpected termination. Did you mean 'd' to stop?")
-        await recorder.finish()
+    recorder = GPIO.Recorder('actions.pkl', verbosed=True)
+    while 1:
+        p = getch()
+        if p == 'w':
+            await recorder.set_speed_and_dump(V_BASE, V_BASE)
+        elif p == 's':
+            await recorder.set_speed_and_dump(0, 0)
+        elif p == 'a':
+            await recorder.set_speed_and_dump(recorder.lspeed-0.1*V_BASE, recorder.rspeed)
+        elif p == 'd':
+            await recorder.set_speed_and_dump(recorder.lspeed, recorder.rspeed-0.1*V_BASE)
+        elif p == 'q':
+            await recorder.finish()
+            break
             
 if __name__ == '__main__':
     asyncio.run(main())
