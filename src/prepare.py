05663579b3db178cf28eedd6842966ff91c85dca
arielherself <ariel@arielherself.xyz>
2023-06-05 13:56:30 +0800

    fix: speed lower than 0

--- src/prepare.py
+++ src/prepare.py
@@ -7,20 +7,24 @@ VR_BASE = 27
 RATIO = 0.1
 
 async def main():
-    recorder = GPIO.Recorder('actions.pkl', verbosed=True)
-    while 1:
-        p = getch()
-        if p == 'w':
-            await recorder.set_speed_and_dump(VL_BASE, VR_BASE)
-        elif p == 's':
-            await recorder.set_speed_and_dump(0, 0)
-        elif p == 'a':
-            await recorder.set_speed_and_dump(recorder.lspeed-RATIO*VL_BASE, recorder.rspeed)
-        elif p == 'd':
-            await recorder.set_speed_and_dump(recorder.lspeed, recorder.rspeed-RATIO*VR_BASE)
-        elif p == 'q':
-            await recorder.finish()
-            break
+    try:
+        recorder = GPIO.Recorder('actions.pkl', verbosed=True)
+        while 1:
+            p = getch()
+            if p == 'w':
+                await recorder.set_speed_and_dump(VL_BASE, VR_BASE)
+            elif p == 's':
+                await recorder.set_speed_and_dump(0, 0)
+            elif p == 'a':
+                await recorder.set_speed_and_dump(max(recorder.lspeed-RATIO*VL_BASE, 0.0), recorder.rspeed)
+            elif p == 'd':
+                await recorder.set_speed_and_dump(recorder.lspeed, max(recorder.rspeed-RATIO*VR_BASE, 0.0))
+            elif p == 'q':
+                await recorder.finish()
+                break
+    except:
+        print('Unexpected interrupt.')
+        await recorder.finish()
             
 if __name__ == '__main__':
     asyncio.run(main())
